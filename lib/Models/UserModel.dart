a0c3330470bfbbad57e01d43c136e8c25b02c8e5
eronsalihu <eron821@gmail.com>
2021-05-19 22:04:08 +0200

    Project updated

--- lib/Models/UserModel.dart
+++
@@ -1,9 +0,0 @@
-class UserModel{
-  int id;
-  String firstName;
-  String lastName;
-  String email;
-  String avatar;
-
-  UserModel(this.id, this.firstName, this.lastName, this.email,this.avatar);
-}
